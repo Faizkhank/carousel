e68e997e2b77935448e6bfd436c9e2ea2693bf58
faiz khan <fine7production@gmail.com>
2023-04-30 16:38:06 +0530

    commit

---
+++ src/Component/Carousel.jsx
@@ -0,0 +1,215 @@
+import React, { useEffect, useState, useRef } from "react";
+import data from "./Store";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Grid,
+  Typography,
+  ImageListItem,
+  ImageList,
+} from "@mui/material";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [SlideAuto, setSlideAuto] = useState(false);
+  const imageListRef = useRef(null);
+  useEffect(() => {
+    if (SlideAuto) {
+      const slideInterval = setInterval(() => {
+        handleNextClick();
+      }, 3000);
+
+      return () => clearInterval(slideInterval);
+    }
+  }, [SlideAuto, activeIndex, data.length, 3000]);
+  const handleNextClick = () => {
+    const imageListEl = imageListRef.current;
+    const itemWidth = imageListEl.scrollWidth / data.length;
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === data.length - 1) {
+        imageListEl.scrollTo({ left: 0, behavior: "smooth" });
+        return 0;
+      } else {
+        imageListEl.scrollLeft += itemWidth;
+        return prevIndex + 1;
+      }
+    });
+  };
+  const handlePrevClick = () => {
+    const imageListEl = imageListRef.current;
+    const itemWidth = imageListEl.scrollWidth / data.length;
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        imageListEl.scrollTo({
+          left: imageListEl.scrollWidth,
+          behavior: "smooth",
+        });
+        return data.length - 1;
+      } else {
+        imageListEl.scrollLeft -= itemWidth;
+        return prevIndex - 1;
+      }
+    });
+  };
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}
+    >
+      <Grid width={{ lg: "85%", sm: "90%", xs: "95%" }} height="auto">
+        <Box sx={{ display: "flex", borderRadius: "10px", boxShadow: "none" }}>
+          <Box
+            sx={{
+              width: "70%",
+              borderRadius: "20px",
+              overflow: "hidden",
+            }}
+          >
+            <Box
+              height={{ xs: 400, sm: 550, md: 600, lg: 700 }}
+              sx={{
+                width: "100%",
+                borderRadius: "20px",
+                overflow: "hidden",
+                "& img": {
+                  width: "100%",
+                  height: "95%",
+                  objectFit: "cover",
+                  borderRadius: "25px",
+                },
+              }}
+            >
+              <img src={data[activeIndex].Image} alt="image" />
+            </Box>
+            <Box sx={{ display: "flex", width: "100%" }}>
+              <Button
+                onClick={() => {
+                  handlePrevClick();
+                }}
+              >
+                <ArrowLeftIcon
+                  sx={{ width: 100, height: 100, color: "black" }}
+                />
+              </Button>
+              <ImageList
+                sx={{
+                  gridAutoFlow: "column",
+                  gridTemplateColumns:
+                    "repeat(auto-fit, minmax(160px,1fr)) !important",
+                  gridAutoColumns: "minmax(160px, 1fr)",
+                  scrollBehavior: "smooth",
+                  "&::-webkit-scrollbar": {
+                    display: "none",
+                  },
+                }}
+                ref={imageListRef}
+              >
+                {data.map((items, index) => (
+                  <ImageListItem key={index}>
+                    <CardMedia
+                      onClick={() => {
+                        setActiveIndex(index);
+                      }}
+                      style={
+                        activeIndex === index
+                          ? { filter: "grayscale(0%)" }
+                          : { filter: "grayscale(100%)" }
+                      }
+                      sx={{
+                        width: 160,
+                        height: 160,
+                        objectFit: "contain",
+                        borderRadius: "20px",
+                        margin: "5px",
+                      }}
+                      image={items.Image}
+                      alt="image"
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+              <Button
+                onClick={() => {
+                  handleNextClick();
+                }}
+              >
+                <ArrowRightIcon
+                  sx={{ width: 100, height: 100, color: "black" }}
+                />
+              </Button>
+            </Box>
+          </Box>
+
+          <Box
+            height={{ xs: 240, sm: 550, md: 600, lg: 700 }}
+            paddingLeft={{ xs: 0, lg: 10 }}
+            sx={{ justifyContent: "center", width: "40%" }}
+          >
+            <Typography
+              sx={{
+                padding: 2,
+                fontSize: 40,
+                color: "grey",
+                overflow: "hidden",
+                height: 50,
+              }}
+            >
+              {data[activeIndex].Title}
+            </Typography>
+            <Box
+              height={{ xs: 290, sm: 440, md: 500, lg: 580 }}
+              sx={{ overflow: "clip" }}
+            >
+              <Typography
+                sx={{
+                  width: "100%",
+                  padding: 2,
+                  overflow: "hidden",
+                  height: "auto",
+                  color: "grey",
+                }}
+              >
+                {data[activeIndex].Desc}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                sx={{
+                  borderRadius: "100%",
+                  height: 120,
+                  width: 120,
+                  padding: 0,
+                  marginTop: 8,
+                }}
+                onClick={() => {
+                  setSlideAuto(!SlideAuto);
+                }}
+              >
+                {!SlideAuto ? (
+                  <PlayCircleFilledWhiteIcon
+                    sx={{ width: 120, height: 120, color: "#00FFFF" }}
+                  />
+                ) : (
+                  <PauseCircleIcon
+                    sx={{ width: 120, height: 120, color: "#00FFFF" }}
+                  />
+                )}
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Grid>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/Component/Store.js
@@ -0,0 +1,58 @@
+const data = new Object([
+  {
+    Image:
+      "https://images.pexels.com/photos/15884963/pexels-photo-15884963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "A random paragraph",
+    Desc: "A random paragraph can also be an excellent way for a writer to tackle writers' block. Writing block can often happen due to being stuck with a current project that the writer is trying to complete. By inserting a completely random paragraph from which to begin, it can take down some of the issues that ",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/16511744/pexels-photo-16511744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Lorem ipsum",
+    Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/14712543/pexels-photo-14712543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Vitae elementum",
+    Desc: "Vitae elementum curabitur vitae nunc sed. Arcu non sodales neque sodales ut etiam sit. Sit amet nulla facilisi morbi tempus iaculis urna. Turpis egestas pretium aenean pharetra magna. Orci ac auctor augue mauris augue. Aenean pharetra magna ac placerat. Turpis tincidunt id aliquet risus feugiat in ante metus. Fusce ut placerat orci nulla pellentesque dignissim enim sit. Odio aenean sed adipiscing diam. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Suspendisse sed nisi lacus sed viverra.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    Title: "Facilisi etiam",
+    Desc: "Facilisi etiam dignissim diam quis. Proin libero nunc consequat interdum varius sit amet mattis vulputate. At risus viverra adipiscing at in tellus integer. In hac habitasse platea dictumst vestibulum. Iaculis urna id volutpat lacus laoreet non. Lectus nulla at volutpat diam ut. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Netus et malesuada fames ac turpis egestas sed tempus urna. Tortor condimentum lacinia quis vel eros donec ac. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Ultricies integer quis auctor elit sed. In fermentum posuere urna nec tincidunt praesent semper. Accumsan tortor posuere ac ut. Ultricies tristique nulla aliquet enim tortor at auctor urna. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est. Odio pellentesque diam volutpat commodo sed egestas egestas.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Felis imperdiet",
+    Desc: "Felis imperdiet proin fermentum leo. Tellus orci ac auctor augue mauris augue neque gravida. Ac auctor augue mauris augue neque. Ipsum consequat nisl vel pretium lectus quam id leo in. Tristique senectus et netus et malesuada fames ac turpis. Tellus orci ac auctor augue mauris augue. Etiam tempor orci eu lobortis. Posuere morbi leo urna molestie at. Vulputate enim nulla aliquet porttitor lacus. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Non quam lacus suspendisse faucibus interdum posuere. Tellus orci ac auctor augue mauris. Nec ullamcorper sit amet risus nullam eget. Lobortis scelerisque fermentum dui faucibus in ornare. Mi sit amet mauris commodo. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Vestibulum lorem sed risus ultricies tristique. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Fringilla est ullamcorper eget nulla facilisi etiam. Eu sem integer vitae justo.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Euismod elementum",
+    Desc: "Euismod elementum nisi quis eleifend quam adipiscing vitae proin. Sit amet volutpat consequat mauris nunc. Vulputate odio ut enim blandit volutpat. Mauris pharetra et ultrices neque ornare aenean euismod elementum. Eu volutpat odio facilisis mauris sit. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Nec dui nunc mattis enim ut tellus elementum sagittis. Maecenas pharetra convallis posuere morbi leo. Rhoncus urna neque viverra justo nec ultrices. Leo in vitae turpis massa sed. Tellus elementum sagittis vitae et.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Adipiscing elit",
+    Desc: "Adipiscing elit ut aliquam purus sit amet. Eu scelerisque felis imperdiet proin fermentum. Enim lobortis scelerisque fermentum dui. Viverra adipiscing at in tellus integer feugiat. Gravida in fermentum et sollicitudin ac. Mi ipsum faucibus vitae aliquet nec ullamcorper sit. Sed vulputate mi sit amet mauris commodo. Gravida neque convallis a cras semper auctor. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Ultrices tincidunt arcu non sodales. Facilisis gravida neque convallis a cras semper auctor neque vitae.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/2440021/pexels-photo-2440021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Magna fermentum",
+    Desc: "Magna fermentum iaculis eu non diam phasellus. Turpis massa sed elementum tempus. Sed faucibus turpis in eu mi bibendum neque egestas. Sed viverra tellus in hac habitasse. Pellentesque id nibh tortor id aliquet lectus. Amet dictum sit amet justo donec enim diam vulputate ut. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Erat imperdiet sed euismod nisi porta lorem. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Eu non diam phasellus vestibulum.",
+  },
+  {
+    Image:
+      "https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    Title: "Tristique senectus",
+    Desc: "Tristique senectus et netus et. Porta non pulvinar neque laoreet suspendisse. Aliquam etiam erat velit scelerisque. Pulvinar sapien et ligula ullamcorper malesuada proin libero. Habitasse platea dictumst vestibulum rhoncus est pellentesque. Ornare quam viverra orci sagittis eu volutpat. Molestie at elementum eu facilisis sed. Ultrices sagittis orci a scelerisque purus semper eget duis at. Lectus quam id leo in vitae turpis massa sed. Egestas diam in arcu cursus euismod quis viverra. Duis at tellus at urna condimentum. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Vitae congue eu consequat ac felis donec et odio. Accumsan sit amet nulla facilisi morbi tempus iaculis urna.",
+  },
+]);
+
+export default data;
